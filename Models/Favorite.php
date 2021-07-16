4a887bb8163aea1d56602494883c51e41b8d972d
makutaramutuma <82492841+makutaramutuma@users.noreply.github.com>
2021-07-16 21:36:15 +0900

    Update Favorite.php

--- Models/Favorite.php
+++ Models/Favorite.php
@@ -29,14 +29,14 @@ class favorite
   public function favoriteDone($company_id,$intern_id)
   {
     $sql = 'INSERT INTO favorite(company_id, intern_id)
-            VALUES (:company_id, :post_id)';
+            VALUES (:company_id, :intern_id)';
 
     try {
     $pdo = connect();
     $pdo->beginTransaction();
     $stmt = $pdo->prepare($sql);
     $stmt->bindValue(':company_id', $company_id, PDO::PARAM_INT);
-    $stmt->bindValue(':post_id', $intern_id, PDO::PARAM_INT);
+    $stmt->bindValue(':intern_id', $intern_id, PDO::PARAM_INT);
     $favorite = $stmt->execute();
     $pdo->commit();
     return $favorite;
@@ -49,14 +49,14 @@ class favorite
 
   public function favoriteCancal($company_id,$intern_id)
   {
-    $sql = 'DELETE FROM favorite WHERE company_id = :company_id AND post_id = :post_id';
+    $sql = 'DELETE FROM favorite WHERE company_id = :company_id AND intern_id = :intern_id';
 
     try {
     $pdo = connect();
     $pdo->beginTransaction();
     $stmt = $pdo->prepare($sql);
     $stmt->bindValue(':company_id', (int)$company_id, PDO::PARAM_INT);
-    $stmt->bindValue(':post_id', (int)$intern_id, PDO::PARAM_INT);
+    $stmt->bindValue(':intern_id', (int)$intern_id, PDO::PARAM_INT);
     $favorite = $stmt->execute();
     $pdo->commit();
     return $favorite;
@@ -69,10 +69,10 @@ class favorite
 
   public function getFavoriteById($company_id)
   {
-    $sql = 'SELECT f.id AS お気に入りID, f.company_id AS 企業ID, f.post_id AS リストID, p.id AS p_id, p.title, c.id AS c_id
-            FROM posts p
+    $sql = 'SELECT f.id AS お気に入りID, f.company_id AS 企業ID, f.post_id AS リストID, i.id AS i_id, i.title, c.id AS c_id
+            FROM intern i
             INNER JOIN favorites f
-            ON f.post_id = p.id
+            ON f.post_id = i.id
             INNER JOIN companies c
             ON f.company_id = c.id
             WHERE f.company_id = :company_id';
